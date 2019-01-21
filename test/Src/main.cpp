2c3ebc74ce257be487c1945ea06c8c4f99588e6f
Hyeonki Hong <hhk7734@gmail.com>
2019-01-21 14:38:18 +0900

    timer2

--- test/Src/main.cpp
+++ test/Src/main.cpp
@@ -119,7 +119,7 @@ int main( void )
     MX_GPIO_Init();
     MX_USART1_UART_Init();
     MX_USART2_UART_Init();
-    MX_TIM1_Init();
+  MX_TIM2_Init();
     MX_I2C1_Init();
     MX_SPI1_Init();
     /* USER CODE BEGIN 2 */
